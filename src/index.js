899e1c6eec728a8ff6bd41b16d8ec0122aceaca8
SimbaWANGU <simbawangulu@gmail.com>
2022-05-10 15:16:36 +0300

    Complete project setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import './style.css';
+import { mainContainer, title, mainSection, scoresDisplay, addScoresDisplay, headingDiv, subTitle, refreshButton, subTitle2, unorderedList, nameInput, scoreInput, submitButton, footer } from './modules/declarations.js';
 
 function component() {
-  const element = document.createElement('div');
-  
-  element.innerHTML = 'Hello Web Pack';
-  element.classList.add('hello');
- 
+  mainContainer.appendChild(title)
 
-  return element;
+  headingDiv.appendChild(subTitle)
+  headingDiv.appendChild(refreshButton)
+
+  scoresDisplay.appendChild(headingDiv)
+  scoresDisplay.appendChild(unorderedList)
+
+  mainSection.appendChild(scoresDisplay);
+
+  addScoresDisplay.appendChild(subTitle2);
+  addScoresDisplay.appendChild(nameInput);
+  addScoresDisplay.appendChild(scoreInput);
+  addScoresDisplay.appendChild(submitButton)
+
+  mainSection.appendChild(addScoresDisplay);
+
+  mainContainer.appendChild(mainSection);
+  mainContainer.appendChild(footer)
+  return mainContainer;
 }
 
 document.body.appendChild(component());
