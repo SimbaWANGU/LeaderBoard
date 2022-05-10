c146f3500d41eb8485a357023c7c4051d64474ee
SimbaWANGU <simbawangulu@gmail.com>
2022-05-10 15:26:02 +0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
 import './style.css';
-import { mainContainer, title, mainSection, scoresDisplay, addScoresDisplay, headingDiv, subTitle, refreshButton, subTitle2, unorderedList, nameInput, scoreInput, submitButton, footer } from './modules/declarations.js';
+import {
+  mainContainer,
+  title,
+  mainSection,
+  scoresDisplay,
+  addScoresDisplay,
+  headingDiv,
+  subTitle,
+  refreshButton,
+  subTitle2,
+  unorderedList,
+  nameInput,
+  scoreInput,
+  submitButton,
+  footer,
+} from './modules/declarations.js';
 
 function component() {
-  mainContainer.appendChild(title)
+  mainContainer.appendChild(title);
 
-  headingDiv.appendChild(subTitle)
-  headingDiv.appendChild(refreshButton)
+  headingDiv.appendChild(subTitle);
+  headingDiv.appendChild(refreshButton);
 
-  scoresDisplay.appendChild(headingDiv)
-  scoresDisplay.appendChild(unorderedList)
+  scoresDisplay.appendChild(headingDiv);
+  scoresDisplay.appendChild(unorderedList);
 
   mainSection.appendChild(scoresDisplay);
 
   addScoresDisplay.appendChild(subTitle2);
   addScoresDisplay.appendChild(nameInput);
   addScoresDisplay.appendChild(scoreInput);
-  addScoresDisplay.appendChild(submitButton)
+  addScoresDisplay.appendChild(submitButton);
 
   mainSection.appendChild(addScoresDisplay);
 
   mainContainer.appendChild(mainSection);
-  mainContainer.appendChild(footer)
+  mainContainer.appendChild(footer);
   return mainContainer;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -16,31 +16,31 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-     publicPath: '/',
+    clean: true,
+    publicPath: '/',
   },
   module: {
-   rules: [
-     {
-       test: /\.css$/i,
-       use: ['style-loader', 'css-loader'],
-     },
-     {
-       test: /\.(png|svg|jpg|jpeg|gif)$/i,
-       type: 'asset/resource',
-     },
-     {
-       test: /\.(woff|woff2|eot|ttf|otf)$/i,
-       type: 'asset/resource',
-     },
-     {
-       test: /\.(csv|tsv)$/i,
-       use: ['csv-loader'],
-     },
-     {
-       test: /\.xml$/i,
-       use: ['xml-loader'],
-     },
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
     ],
   },
 };
