83f899a79fe717eb908677cf09eb634bf3991036
SimbaWANGU <simbawangulu@gmail.com>
2022-05-11 12:25:19 +0300

    Consume leader api with async and await

---
+++ src/modules/postGame.js
@@ -0,0 +1,13 @@
+const postGame = async (userName, userScore) => {
+    const values = JSON.stringify({ 
+        user: userName,
+        score: userScore
+    });
+    await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/CDJ8oUTB9lzH5vIwdnUz/scores', {
+        method: "POST",
+        headers: { 'Content-Type':'application/json' },
+        body: values
+	})
+}
+
+export default postGame;
